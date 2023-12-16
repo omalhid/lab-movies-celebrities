d45241ee2821fb26e358b3620f82dda99bc8df1c
Omayma El Hadari <om.elhadari@gmail.com>
2023-12-16 12:50:23 +0100

    iterations 1-5

---
+++ models/Movie.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+const Celebrity = require('/models/Celebrity.model')
+
+const movieSchema = new Schema({
+    title: {
+        type:String,
+        unique:true },
+    genre: String,
+    plot: String,
+    cast:{
+        type: ObjectId,
+        ref:'Celebrity'
+    }
+})
+
+const Movie = mongoose.model ("Movie", movieSchema);
+module.exports = Movie;
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,41 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+const Celebrity = require('../models/Celebrity.model')
+
+// all your routes here
+//GET route for displaying the form to create a celebrity
+router.get('/celebrities/create', (req, res, next) => {
+    res.render('celebrities/new-celebrity')
+})
+
+//POST route to submit the created Celebrity
+router.post('/celebrities/create', (req,res,next) => {
+    const {name, occupation, catchPhrase} = req.body;
+    Celebrity.findOne({name})
+    .then((celebrityFromDB) => {
+        if(!celebrityFromDB){
+            Celebrity.create(req.body)
+            .then(()=>res.redirect('/celebrities/celebrities'));
+        }else{
+            res.render('celebrities/new-celebrity', {
+                message: 'Celebrity already registered in the database'
+            });
+            return;
+        }
+    })
+    .catch((err) => {
+        console.log('error occurred while adding your celebrity', err);
+    })
+})
+
+//GET route to display the loist of celebrities
+router.get('/celebrities', (req, res, next) => {
+    Celebrity.find()
+    .then((listOfCelebrities)=>{
+        console.log('list of celebrities', listOfCelebrities);
+        res.render('/celebrities/celebrities')
+    })
+    .catch((err)=> console.log('error occurred while listing the celebrities ', err))
+})
+
+module.exports = router;
